9bcee323db8461620783c03995ae54dbfb27133f
unknown <angelo.milcevski@gmail.com>
2017-12-26 15:14:12 +0100

    Add Post migration, model, controller and sluggable feature

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class IndexController extends Controller
 {
@@ -13,9 +14,18 @@ class IndexController extends Controller
      */
     public function index()
     {
-        return view('index');
+		$posts = Post::orderBy('created_at', 'DESC')->paginate(20);
+		
+        return view('index')->with('posts', $posts);
+		
     }
 
+	 /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
 
      
     public function show($id)
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,5 +5,6 @@
 @section('content')
 <div class="row">
 
+
 </div>
 @stop
